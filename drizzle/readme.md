8156a5041558f79479951955ad2fb62052430d2f
caob1234 <q799229025@163.com>
2017-09-13 15:00:35 +0800

    测试drizzlejs暴漏出来的方法

---
+++ drizzle/readme.md
@@ -0,0 +1,5 @@
+drizzlejs向外部暴漏出来的方法，也就是放到D变量中的function有四个，分别是：
+assign：用于将两组数据相加，并返回相加之后的数据。不受array，object，function类型限制。
+extend：继承。
+adapt：只有一个参数obj，利用assign将obj叠加到D.Adapt里面。
+uniqueId：
--- drizzle/testMap.js
+++ drizzle/testMap.js
@@ -1,1 +1,14 @@
-const drizzle = require('../drizzlejs');
+const D = require('drizzlejs');
+
+
+let array = [{a:1},{b:2}];
+let array1 = [];
+
+console.log(D.isArray(array));
+console.log(array1);
+console.log(D.assign(array1,array));
+console.log(array1);
+let obj1 = {c:3};
+console.log(D.assign(array1,obj1));
+console.log(array1);
+
