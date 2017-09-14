7f2b03d6986654771334948fc3e806c66d2d08a4
caob1234 <q799229025@163.com>
2017-09-14 10:38:03 +0800

    增加gulp-util为日志打印方法

--- drizzle/testMap.js
+++ drizzle/testMap.js
@@ -1,14 +1,15 @@
 const D = require('drizzlejs');
+const gutil = require('gulp-util');
 
 
 let array = [{a:1},{b:2}];
 let array1 = [];
 
-console.log(D.isArray(array));
-console.log(array1);
-console.log(D.assign(array1,array));
-console.log(array1);
+gutil.log(D.isArray(array));
+gutil.log(array1);
+gutil.log(D.assign(array1,array));
+gutil.log(array1);
 let obj1 = {c:3};
-console.log(D.assign(array1,obj1));
-console.log(array1);
+gutil.log(D.assign(array1,obj1));
+gutil.log(array1);
 
