eed307ff5d172d4567f6c08020c3720a841d0d12
caob1234 <q799229025@163.com>
2019-05-31 21:26:01 +0800

    test Generator

---
+++ ESsix/testGenerator.js
@@ -0,0 +1,22 @@
+const gutil = require('gulp-util'),
+    path = require('path'),
+    fs = require('fs');
+const readFile = function (fileName) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(fileName, function (error, data) {
+            if (error) return reject(error);
+            resolve(data);
+        });
+    });
+};
+
+const gen = async function () {
+    const f1 = await readFile(path.dirname(require.main.filename)+'/readme.md');
+    const f2 = await readFile(path.dirname(require.main.filename)+'/Vehicle.js');
+    gutil.log(f1.toString());
+    gutil.log(f2.toString());
+};
+
+gen();
+
+//Generator函数在目前的版本貌似已经不支持
