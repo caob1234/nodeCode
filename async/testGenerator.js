ff72e3ed13eb764c78b87d4cb8c9eaab65a176ad
grassmonkey <q799229025@163.com>
2020-04-10 19:19:10 +0800

    after async

--- async/testGenerator.js
+++ async/testGenerator.js
@@ -18,20 +18,21 @@ const readFile = function (fileName) {
 // gen().next().value.then(data=>{gutil.log(data.toString())});
 // gen().next().value.then(data=>gutil.log(data.toString()));
 //test 2
-function* gen1() {
-    yield readFile(path.dirname(require.main.filename) + '/readme.md');
-    yield readFile(path.dirname(require.main.filename) + '/exerciseAsync.js');
-    // gutil.log(f1.toString());
-    // gutil.log(f2.toString());
-};
-let gen = gen1();
-gen.next().value.then(data => gutil.log(data.toString()));
-gen.next().value.then(data => gutil.log(data.toString()));
+// function* gen1() {
+//     yield readFile(path.dirname(require.main.filename) + '/readme.md');
+//     yield readFile(path.dirname(require.main.filename) + '/exerciseAsync.js');
+//     // gutil.log(f1.toString());
+//     // gutil.log(f2.toString());
+// };
+// let gen = gen1();
+// gen.next().value.then(data => gutil.log(data.toString()));
+// gen.next().value.then(data => gutil.log(data.toString()));
 //test 3
-// let gen=async function(){
-//     let r1=await readFile(path.dirname(require.main.filename)+'/readme.md');
-//     let r2=await readFile(path.dirname(require.main.filename)+'/exerciseAsync.js');
-//     gutil.log(r1.toString())
-//     gutil.log(r2.toString())
-// }
-// gen();
+let asyncF=async function(){
+    let r1=await readFile(path.dirname(require.main.filename)+'/readme.md');
+    let r2=await readFile(path.dirname(require.main.filename)+'/exerciseAsync.js');
+    gutil.log(r1.toString())
+    gutil.log(r2.toString())
+    return "hello async";
+}
+asyncF().then(data=>gutil.log(data));
